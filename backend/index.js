18faeac2d2a7b76801e2a4a0b626b9a15469fd5a
Thepranaypatil <patilpranayy@gmail.com>
2026-03-05 23:19:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,11 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+  /* origin: process.env.CLIENT_URL || "http://localhost:5173",*/
+     origin: [
+      "http://localhost:5173",
+      "https://ecommerce-ekwp.vercel.app",
+    ],
 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
